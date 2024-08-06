3c2c0a7c9cb915cbe3a352978bda5edb0467284f
Shubham Gupta <shubham272101@gmail.com>
2024-08-06 23:53:55 +0530

    add components for signin and signup

--- learn_next/app/signup/page.tsx
+++ learn_next/app/signup/page.tsx
@@ -1,7 +1,7 @@
+import { SignupComponent } from "@/components/SignupComponent";
+
 export default function Signup(){
     return <div>
-        <div className="flex justify-center font-extrabold text-2xl p-4">
-            Signup Page
-        </div>
+        <SignupComponent />
     </div>
 }
---
+++ learn_next/components/SignupComponent.tsx
@@ -0,0 +1,7 @@
+export const SignupComponent = function(){
+    return <div>
+        <div className="flex justify-center font-extrabold text-2xl p-4">
+            Signup Page
+        </div>
+    </div>
+}
