eb9a57fc3959963aac7052898d2b80e5ccc7f495
Shubham Gupta <shubham272101@gmail.com>
2024-08-06 19:56:20 +0530

    changes in navbar and parent layout

--- learn_next/components/Navbar.tsx
+++ learn_next/components/Navbar.tsx
@@ -1,10 +1,18 @@
 export const Navbar = () =>{
-    return <div className="font-mono p-4 border-2 flex items-center text-xl">
+    return <div className="font-mono p-4 border-2 flex justify-center text-xl">
         <div className="font-extrabold flex-1">
             Learn
         </div>
-        <div className="font-bold">
-            Home
+        <div className="flex justify-center gap-16">
+            <div className="font-bold">
+                Home
+            </div>
+            <div className="font-bold">
+                About Us
+            </div>
+            <div className="font-bold">
+                Help
+            </div>
         </div>
     </div>
 }
